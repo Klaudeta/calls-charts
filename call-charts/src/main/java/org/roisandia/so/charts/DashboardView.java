36f07f5c25474384e15068765e40f0820232bfd6
Klaudeta <klaudeta@vaadin.com>
2018-08-05 15:06:29 +0300

    add vaadin-board and vaadin-chart

--- call-charts/src/main/java/org/roisandia/so/charts/DashboardView.java
+++ call-charts/src/main/java/org/roisandia/so/charts/DashboardView.java
@@ -1,6 +1,7 @@
 package org.roisandia.so.charts;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
@@ -14,4 +15,15 @@ public class DashboardView extends PolymerTemplate<TemplateModel> {
 
   @Id
   private TopMenu topMenu;
+
+  @Id("deliveriesThisMonth")
+  private Chart callsThisMonthChart;
+
+  @Id("deliveriesThisYear")
+  private Chart callsThisYearChart;
+
+  public DashboardView() {
+
+    // TODO 1. call chart service/presenter to create Chart Cards
+  }
 }
